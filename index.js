2dddd3d036eeb16b22fa2fcf27feed1899cb1ef3
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-31 14:47:30 -0300

    Add get /info (ex3.2)

--- index.js
+++ index.js
@@ -33,6 +33,15 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const numberOfPersons = persons.length;
+  const date = new Date();
+  res.send(`
+    <p>Phonebook has info for ${numberOfPersons} people</p>
+    <p>${date}</p>
+    `);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
