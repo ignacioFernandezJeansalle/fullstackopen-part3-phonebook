5029bfcf055f87b9840a2416b6d82f31f4875222
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-06-11 10:50:35 -0300

    Create Person model of MongoDB

---
+++ models/person.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("Connecting to MongoDB...");
+
+mongoose
+  .connect(url)
+  .then((result) => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("Error connecting to MongoDB: ", error.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
